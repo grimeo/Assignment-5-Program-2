a4cd8cd768dfd98b7e22cf2132170b15922b16bb
Romeo <kilazlove@gmail.com>
2021-11-15 00:20:35 +0800

    finish the code by filtering the inputs using condition statement

--- index.py
+++ index.py
@@ -5,9 +5,15 @@
 # Display the lowest number
 
 
-num1 = float(input("Enter the first number"))
-num2 = float(input("Enter the second number"))
-num3 = float(input("Enter the third number"))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
 
 if num1 < num2 and num1 < num3:
-    print(num1 + " is the lowest number")
+    print(str(num1) + " is the lowest number.")
+elif num2 < num1 and num1 < num3:
+    print(str(num2) + " is the lowest number.")
+elif num3 < num2 and num3 < num1:
+    print(str(num3) + " is the lowest number.")
+else:
+    print("error")
